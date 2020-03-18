fb82f4b7db4abf756888f1943fcfe6af213b7ded
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2020-03-18 11:47:16 +0800

    Update exercise3.py

--- Exercise/exercise3.py
+++ Exercise/exercise3.py
@@ -1,7 +1,53 @@
+import random
+
 # 一、
 # 將撲克牌(52張)亂數分給n個人，n為大於零的整數，每個人都會拿到52/n張撲克牌，印出每一組撲克牌。(hint: random.randint(), List, Dictionary)
 
+# import random
+#
+# suit = ["♠️", "♥️", "♦️", "♣️"]
+# num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+# deck = []
+# for i in suit:
+#     for j in num:
+#         deck.append(i + j)
+# random.shuffle(deck)
+# print(deck)
+#
+# n = int(input('請輸入人數'))
+# cd_count = 52 // n
+#
+# pepo_get = [[0 for j in range(cd_count)] for i in range(n)]
+# for i in range(n):
+#     for j in range(cd_count):
+#         pepo_get[i][j] = deck.pop()
+#
+# for x in range(n):
+#     print('第{}人的牌:'.format(x+1))
+#     print(pepo_get[x])
+#
+# if cd_count != 0:
+#     print('未發出的牌為:', deck)
 
+suit = ['♠', '♥', '♦', '♣']
+num = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+deck = []
+
+# 生成撲克牌
+for i in suit:
+    for j in num:
+        deck.append(i+j)
+
+# 洗牌
+random.shuffle(deck)
+
+n = int(input('請輸入人數'))
+# 要個人幾張牌
+cd_count = 52 // n
+
+people_init = [[0 for j in range(cd_count)] for i in range(n)]
+print(people_init)
 
 
 
